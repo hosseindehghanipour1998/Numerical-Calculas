b1ffff4359158d4068aa04fbfd2a403a4a4df238
Hossein <hossein.dehghanipour1998@gmail.com>
2020-01-20 22:12:17 +0330

    Initial Commit

---
+++ BairstowMethod/Bairswtow.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 22:03:53 2020
+
+@author: Hossein
+"""
+
+def main():
+    n = input("Enter Polynomial's Degree : ")
+    inputCoefficients = input().split(' ')
+    print(inputCoefficients)
+    
+main()
