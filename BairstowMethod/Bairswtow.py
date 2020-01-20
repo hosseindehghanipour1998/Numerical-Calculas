a87381eff7acc9641097b109ee35d35abffa7938
Hossein <hossein.dehghanipour1998@gmail.com>
2020-01-21 02:41:07 +0330

    All required Functionalities are added and fully tested.-> don't fotget to umcomment the degree of polynomial as input

--- BairstowMethod/Bairswtow.py
+++ BairstowMethod/Bairswtow.py
@@ -37,29 +37,35 @@ def bairstow( roots_list , coefficients_list , r0 , s0 ):
         
         while(True):
             b = [0] * ( n + 1 )
+            
             #Calculate B's
             b[n] = coefficients_list[n]
             b[n-1] = coefficients_list[n-1] + r0 * b[n]
             for i in range(n-2 , -1 , -1):
                 b[i] = coefficients_list[i] + r0 * b[i+1] + s0 * b[i+2]
+            
             #Calculate C's
             c = [0] * ( n + 1 )
             c[n] = b[n]
             c[n-1] = b[n-1] + r0 * c[n] 
             for i in range(n-2 , -1 , -1):
                 c[i] = b[i] + r0 * c[i+1] + s0 * c[i+2]
+           
             #Calculating D's
             D  = calculateDeterminant (c[1] , c[2] , c[2] , c[3])
             D1 = calculateDeterminant (-b[0] , c[2] , -b[1] , c[3])
             D2 = calculateDeterminant (c[1] , -b[0] , c[2] , -b[1])
             
+            #Delta
             delta_r = float(D1/D)
             r0 = r0 + delta_r
             delta_s = float(D2/D)
             s0 = s0 + delta_s
             
+            #check if the difference is greater than our tolerance (epsilon) or not
             if ( abs(b[0]) < tolerance and abs(b[1]) < tolerance ):
                 break
+        
         #End of While
         dis = (r0)**(2.0) + (4.0 * (s0))
         x1 = (r0 - (mathematician.sqrt(dis)))/(2)
@@ -68,7 +74,7 @@ def bairstow( roots_list , coefficients_list , r0 , s0 ):
         roots_list.append(x2)
         return bairstow(roots_list , b[2:] , r0 , s0 )
     
-################### MATRIX OPERATIONS #################33
+###################  Utility Functions #################33
 
 def calculateDeterminant(index00 , index01 , index10 , index11 ):
     return ( index00 * index11 - index01*index10 ) 
@@ -86,28 +92,46 @@ def sigma(a_list):
     for number in a_list :
         if(number.imag > 0 or numpy.iscomplex(number) == False):
             summation += number
-            print("Positive : " , number)
     return summation
 
+def printOutput(a_complexNumber):
+    realPart = a_complexNumber.real
+    imaginaryPart = a_complexNumber.imag
+    print('%.6f '%realPart ," ", '%.6f'%imaginaryPart )
 ############## MAIN #####################
+    '''
+    -3x^5 + 2x^4 + x^3 + 0x^2 - x - 1
+    #Sample Input : inputCoefficients = [-3 , 2 , 1 , 0 , -1 , -1]
+    '''
 def main():
-    #INPUT 
+    #Get Input : 
     #n = input("Enter Polynomial's Degree : ")
-    #inputCoefficients = list(reversed(input().split(' ')))
-    inputCoefficients = [-3,2, 1, 0, -1 ,-1]
-    inputCoefficients = [-1,-1,0,1,2,-3]
-    #inputCoefficients = atoi(inputCoefficients)
+    print("Sample -> Ax^2 + Bx + C => A B C" )
+    print("Enter The Coefficients as said Above: ")
+    inputCoefficients = list(reversed(input().split(' ')))
+    inputCoefficients = atoi(inputCoefficients)
     
-    #NEEDED ELEMENTS
+    #Initial Elemnets and Values
     roots_list = [] 
-    s0 = 0.1
-    r0 = 0.1
-    bairstow(roots_list , inputCoefficients , r0 , s0)
+    s0 = random.random()
+    r0 = random.random()
+    
+    #Printing Coefficients
     print("Input Coefficints : " , inputCoefficients)
+    print("===========================")
+    
+   #Main Function Call
+    bairstow(roots_list , inputCoefficients , r0 , s0)
+    
+    #Printing Calculated Roots
     printList("Roots : " , roots_list)
+    print("===========================")
+    
+    #Printing the output Summation
+    print("Output : ( Positive Roots Summation) ")
     summation = sigma(roots_list)
-    print(summation)
-
+    printOutput(summation)
+########################################## RUN #############   
     
 main() 
 
